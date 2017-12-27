4517fc119ea5a054992792ad6b6f54b4fa4aa326
Pharrell.zx <15113029g@connect.polyu.hk>
2017-12-27 12:07:17 +0800

    Deploy Next.js to gh-pages

---
+++ _next/2b54361e-d373-4594-83a8-66c1a697dd2f/page/about.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/about', function() {
+            var comp = module.exports=webpackJsonp([5],{317:function(e,t,l){e.exports=l(318)},318:function(e,t,l){"use strict";function u(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var n=l(0),a=u(n),r=l(150),d=u(r);t.default=function(){return a.default.createElement("div",null,a.default.createElement("div",null,"About us"),a.default.createElement("div",null,"Back to ",a.default.createElement(d.default,{href:"/",as:"/nextjs-boilerplate/"},a.default.createElement("a",null,"Home"))))}}},[317]);
+            return { page: comp.default }
+          })
+        
